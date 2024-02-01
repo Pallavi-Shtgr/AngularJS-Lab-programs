86d0eda20aa9957b8218682ee42f07863fbcd72d
Pallavi <pallavishettigar03@gmail.com>
2024-02-01 22:46:28 +0530

    add new files via upload

--- login.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('myApp', []); 
-app.controller('LoginController', function ($scope) { 
-    $scope.user = { username: '', password: '' }; 
-    $scope.isLoggedIn = false; 
-    $scope.login = function ()  
-        $scope.isLoggedIn = true; 
-    }; 
-});
