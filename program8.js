521741f74b8911465be442ab42e68d0aeb82c5b2
Pallavi <pallavishettigar03@gmail.com>
2024-03-10 11:35:21 +0530

    add new js file

---
+++ program8.js
@@ -0,0 +1,10 @@
+
+  var app = angular.module('myApp', []); 
+    app.controller('LoginController', function ($scope) { 
+        $scope.user = { username: '', password: '' }; 
+        $scope.isLoggedIn = false; 
+        $scope.login = function ()  {
+            $scope.isLoggedIn = true; 
+        }; 
+    }); 
+
