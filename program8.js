52762e3907f86f4c318ccb9d4444806a7bff372d
Pallavi <pallavishettigar03@gmail.com>
2024-03-13 07:25:52 +0530

    add new chsnhes

--- program8.js
+++ program8.js
@@ -1,10 +1,1 @@
 
-  var app = angular.module('myApp', []); 
-    app.controller('LoginController', function ($scope) { 
-        $scope.user = { username: '', password: '' }; 
-        $scope.isLoggedIn = false; 
-        $scope.login = function ()  {
-            $scope.isLoggedIn = true; 
-        }; 
-    }); 
-
